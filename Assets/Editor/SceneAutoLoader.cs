20b1aeb8d4ee0cad69e4a2a081c4836d757cfac8
tststs <contact@tobias-schubert.com>
2016-07-20 18:49:24 +0200

    initial commit

---
+++ Assets/Editor/SceneAutoLoader.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+static class SceneAutoLoader {
+
+	static string sceneToLoad = "Assets/Scenes/main-menu.unity";
+	private static string PreviousScenePath {
+
+		get {
+			
+			return EditorPrefs.GetString("SceneAutoLoader.PreviousScenePath", SceneManager.GetActiveScene().path);
+		}
+
+		set {
+
+			EditorPrefs.SetString("SceneAutoLoader.PreviousScenePath", value);
+		}
+	}
+
+	static SceneAutoLoader() {
+
+        EditorApplication.playmodeStateChanged += OnPlayModeChanged;
+    }
+
+    private static void OnPlayModeChanged() {
+
+		if (!EditorApplication.isPlaying && EditorApplication.isPlayingOrWillChangePlaymode) {
+
+			PreviousScenePath = SceneManager.GetActiveScene().path;
+			EditorSceneManager.OpenScene(sceneToLoad);
+
+			return;
+        }
+
+		if (!EditorApplication.isPlaying && !EditorApplication.isPlayingOrWillChangePlaymode) {
+
+			EditorSceneManager.OpenScene(PreviousScenePath);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # UnitySceneAutoLoader
 Auto loads a specific scene on play-mode enter and loads the previously opened scene on play-mode stop
+
+# Installation
+Copy the script to `your-project-path/Assets/Editor`
+
+# Usage
+Open the script and change the value for `sceneToLoad` to the scene you want to load on play
+
+# Hints
+* All scenes have to be registered in `Scenes To Build` within `File/Build Settings...`
+* All scene files have to be located in `Assets/Scenes`
